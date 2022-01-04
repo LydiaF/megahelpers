24b6db55fa1dffa9eb9f2fbfce2e1b97b911f10c
Lydia <lydia@learntogether.eu>
2022-01-04 11:38:02 +0000

    .

--- src/useSelectIds.ts
+++ src/useSelectIds.ts
@@ -1,15 +1,16 @@
-import { map } from 'lodash'
+import { map, difference } from 'lodash'
 import { useState } from 'react'
 
 export const useSelectIds = (incoming, keyField = 'id') => {
-  const [state, setState] = useState(map(incoming, keyField))
+  const original = map(incoming, keyField)
+  const [state, setState] = useState(original)
   const itemOnClick = (object) =>
     state.includes(object[keyField])
       ? setState((s) => s.filter((i) => i !== object[keyField]))
       : setState((s) => [...s, object[keyField]])
 
   return {
-    deselected: [],
+    deselected: difference(original, state),
     selectedToGo: state,
     itemOnClick,
     objectDiff: [true], // COMBAK
